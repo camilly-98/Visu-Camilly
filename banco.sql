c2859e5f9561d8dc4842f2a834d154e8e913620a
camlop2 <camillyht@eaportal.org>
2022-10-28 11:16:43 -0300

    inicial

---
+++ banco.sql
@@ -0,0 +1,267 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 13-Maio-2021 às 16:40
+-- Versão do servidor: 10.4.13-MariaDB
+-- versão do PHP: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `city`
+--
+
+CREATE TABLE `Cidade` (
+  `idCidade` int(11) NOT NULL,
+  `nameCidade` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `Cidade`
+--
+INSERT INTO `Cidade` (`idCidade`, `nameCidade`) VALUES
+(1, 'Campinas'),
+(2, 'Hortolândia'),
+(3, 'Indaiatuba'),
+(4, 'Sumaré'),
+(5, 'Capivari'),
+(6, 'Jundiaí'),
+(7, 'Americana'),
+(8, 'Nova Odessa'),
+(9, 'Botucatu'),
+(10, 'Atibaia'),
+(11, 'Piracicaba'),
+(12, 'Jaguariuna'),
+(13, 'Amparo'),
+(14, 'Limeira');
+
+--
+-- Extraindo dados da tabela `extrato`
+--
+
+CREATE TABLE `extrato` (
+  `ID_extrato` int(11) NOT NULL,
+  `DATA_extrato` datetime DEFAULT current_timestamp(),
+  `TOTAL_PRECO` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `extrato`
+--
+
+INSERT INTO `extrato` (`ID_extrato`, `DATA_extrato`, `TOTAL_PRECO`) VALUES
+(1, '2021-04-27 22:08:04', 160),
+(2, '2020-01-14 12:28:59', 657),
+(3, '2021-04-27 19:19:12', 5000),
+(4, '2021-04-27 20:13:45', 129),
+(5, '2021-04-27 00:48:37', 738);
+
+-- --------------------------------------------------------
+-- Estrutura da tabela `transacoes`
+--
+CREATE TABLE `transacoes` (
+  `ID` int(11) NOT NULL,
+  `FK_PRODUTO` int(11) NOT NULL,
+  `FK_extrato` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `transacoes`
+--
+
+INSERT INTO `transacoes` (`ID`, `FK_PRODUTO`, `FK_extrato`) VALUES
+(1, 3, 4),
+(2, 2, 2),
+(3, 4, 1),
+(4, 5, 3),
+(9, 3, 5),
+(10, 1, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `servicos`
+--
+
+CREATE TABLE `servicos` (
+  `ID_servicos` int(11) NOT NULL,
+  `NOME_servicos` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `servicos`
+--
+
+INSERT INTO `servicos` (`ID_servicos`, `NOME_servicos`) VALUES
+(1, 'Cartões'),
+(2, 'PIX'),
+(3, 'Cheque'),
+(4, 'Empréstimo'),
+(5, 'Recarga'),
+(6, 'Investimentos'),
+(7, 'Transferências'),
+(8, 'Seguro'),
+(9, 'Mensalidade'),
+(10, 'Whatsapp');
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `adm_usuario`
+--
+
+CREATE TABLE `adm_usuario` (
+  `idPerfil` int(11) NOT NULL,
+  `namePerfil` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `adm_usuario`
+--
+
+INSERT INTO `adm_usuario` (`idPerfil`, `namePerfil`) VALUES
+(1, 'Admin'),
+(2, 'User');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `name` varchar(20) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `Cidade` varchar(15) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `id` int(11) NOT NULL,
+  `fk_idPerfil` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`name`, `username`, `password`, `Cidade`, `image`, `gender`, `id`, `fk_idPerfil`) VALUES
+('Camilly Lopes', 'camilly@visucard.bm', 'visu1234', 'knp', 'image/images.png', 'male', 2, NULL),
+('Alciomar Hollanda', 'alciomar@gmail.com', '123', 'knp', 'image/github-octocat.png', 'male', 3, 2);
+
+--
+-- Índices para tabelas despejadas
+--
+-- Índices para tabela `Cidade`
+--
+ALTER TABLE `Cidade`
+  ADD PRIMARY KEY (`idCidade`);
+
+--
+-- Índices para tabela `compra`
+--
+ALTER TABLE `extrato`
+  ADD PRIMARY KEY (`ID_extrato`);
+
+--
+-- Índices para tabela `compra_produto`
+--
+ALTER TABLE `transacoes`
+  ADD PRIMARY KEY (`ID`,`FK_PRODUTO`,`FK_extrato`),
+  ADD KEY `FK_PRODUTO` (`FK_PRODUTO`),
+  ADD KEY `FK_extrato` (`FK_extrato`);
+
+--
+-- Índices para tabela `produto`
+--
+ALTER TABLE `servicos`
+  ADD PRIMARY KEY (`ID_servicos`);
+
+--
+-- Índices para tabela `adm_usuario`
+--
+ALTER TABLE `adm_usuario`
+  ADD PRIMARY KEY (`idPerfil`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_idPerfil` (`fk_idPerfil`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `Cidade`
+--
+ALTER TABLE `Cidade`
+  MODIFY `idCidade` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `compra`
+--
+ALTER TABLE `extrato`
+  MODIFY `ID_extrato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `compra_produto`
+--
+ALTER TABLE `transacoes`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `servicos`
+  MODIFY `ID_servicos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `adm_usuario`
+--
+ALTER TABLE `adm_usuario`
+  MODIFY `idPerfil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `compra_produto`
+--
+ALTER TABLE `transacoes`
+  ADD CONSTRAINT `transacoes_ibfk_1` FOREIGN KEY (`FK_servicos`) REFERENCES `servicos` (`ID_servicos`),
+  ADD CONSTRAINT `transacoes_ibfk_2` FOREIGN KEY (`FK_extrato`) REFERENCES `extrato` (`ID_extrato`);
+
+--
+-- Limitadores para a tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`fk_idPerfil`) REFERENCES `perfil_usuario` (`idPerfil`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ entrar.php
@@ -0,0 +1,211 @@
+<?php
+
+// Conexão com banco de dados 
+include'connect.php';
+
+if(isset($_POST['sub'])){
+    $u=$_POST['text'];
+    $p=$_POST['pass'];
+    $s= "
+    select * from usuario ";   
+    $qu= mysqli_query($con, $s);
+   
+    if(mysqli_num_rows($qu)>0){
+        $f= mysqli_fetch_assoc($qu);
+        $_SESSION['id']=$f['id'];
+        $_SESSION['perfil']=$f['namePerfil'];
+        header ('location:paginainicial.php');
+    }
+   else{
+       echo 'Usuário ou senha inválidos';
+   }
+  
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="soft-ui-dashboard-main/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="soft-ui-dashboard-main/assets/img/favicon.png">
+  <title>
+    Entrar
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="soft-ui-dashboard-main/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="soft-ui-dashboard-main/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="soft-ui-dashboard-main/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="soft-ui-dashboard-main/assets/css/soft-ui-dashboard.css?v=1.0.6" rel="stylesheet" />
+</head>
+
+<body class="">
+  <div class="container position-sticky z-index-sticky top-0">
+    <div class="row">
+      <div class="col-12">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg blur blur-rounded top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+          <div class="container-fluid pe-0">
+            <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="inicio.php">
+              VISUCARD
+            </a>
+            <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon mt-2">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+              </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navigation">
+              <ul class="navbar-nav mx-auto ms-xl-auto me-xl-7">
+                <li class="nav-item">
+                  <a class="nav-link me-2" href="cadastrar.php">
+                    <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
+                    Cadastrar
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link me-2" href="entrar.php">
+                    <i class="fas fa-key opacity-6 text-dark me-1"></i>
+                    Entrar
+                  </a>
+                </li>
+              </ul>
+              
+            </div>
+          </div>
+        </nav>
+        <!-- End Navbar -->
+      </div>
+    </div>
+  </div>
+  <main class="main-content  mt-0">
+    <section>
+      <div class="page-header min-vh-75">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+              <div class="card card-plain mt-8">
+                <div class="card-header pb-0 text-left bg-transparent">
+                  <h3 class="font-weight-bolder text-info text-gradient">Bem-Vindo de volta!</h3>
+                  <p class="mb-0">Insira seu email e senha para entrar</p>
+                </div>
+                <div class="card-body">
+                  <form method="POST" enctype="multipart/form-data" role="form">
+                    <label>Email</label>
+                    <div class="mb-3">
+                      <input type="text" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="email-addon" name="text" value>
+                    </div>
+                    <label>Senha</label>
+                    <div class="mb-3">
+                      <input type="password" id="exampleInputPassword" class="form-control" placeholder="Senha" aria-label="Password" aria-describedby="password-addon" name="pass" value>
+                    </div>
+                    <div class="form-check form-switch">
+                      <input class="form-check-input" type="checkbox" id="rememberMe" checked="">
+                      <label class="form-check-label" for="rememberMe">Lembrar de mim</label>
+                    </div>
+                    <div class="text-center">
+                      <button type="submit" href="paginainicial.php" class="btn bg-gradient-info w-100 mt-4 mb-0" name="sub" value="">Entrar</button>
+                
+                    </div>
+                  </form>
+                </div>
+                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                  <p class="mb-4 text-sm mx-auto">
+                    Não tem uma conta ainda?
+                    <a href="cadastrar.php" class="text-info text-gradient font-weight-bold">Cadastrar</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
+                <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6" style="background-image:url('soft-ui-dashboard-main/assets/img/curved-images/curved6.jpg')"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+  <footer class="footer py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mb-4 mx-auto text-center">
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+            Company
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+            About Us
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+            Team
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+            Products
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+            Blog
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+            Pricing
+          </a>
+        </div>
+        <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+            <span class="text-lg fab fa-dribbble"></span>
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+            <span class="text-lg fab fa-twitter"></span>
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+            <span class="text-lg fab fa-instagram"></span>
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+            <span class="text-lg fab fa-pinterest"></span>
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+            <span class="text-lg fab fa-github"></span>
+          </a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-8 mx-auto text-center mt-1">
+          <p class="mb-0 text-secondary">
+            Copyright © <script>
+              document.write(new Date().getFullYear())
+            </script> Soft by Creative Tim.
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+  <!--   Core JS Files   -->
+  <script src="soft-ui-dashboard-main/assets/js/core/popper.min.js"></script>
+  <script src="soft-ui-dashboard-main/assets/js/core/bootstrap.min.js"></script>
+  <script src="soft-ui-dashboard-main/assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="soft-ui-dashboard-main/assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="soft-ui-dashboard-main/assets/js/soft-ui-dashboard.min.js?v=1.0.6"></script>
+</body>
+
+</html>
