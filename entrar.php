0cf02402bd7bb7905437fb5c7b8db52435ed164d
camlop2 <camillyht@eaportal.org>
2022-10-28 18:46:09 -0300

    ..

--- entrar.php
+++ entrar.php
@@ -93,7 +93,7 @@ if(isset($_POST['sub'])){
             <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
               <div class="card card-plain mt-8">
                 <div class="card-header pb-0 text-left bg-transparent">
-                  <h3 class="font-weight-bolder text-info text-gradient">Bem-Vindo de volta!</h3>
+                  <h3 class="font-weight-bolder text-info text-gradient">Bem-Vindo!</h3>
                   <p class="mb-0">Insira seu email e senha para entrar</p>
                 </div>
                 <div class="card-body">
--- paginainicial.php
+++ paginainicial.php
@@ -35,7 +35,7 @@
   <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href="inicio.php " target="_blank">
+      <a class="navbar-brand m-0" href="inicio.php" target="_blank">
         <span class="navbar-brand font-weight-bolder ms-lg-0 ms-3 ">VISUCARD</span>
         <img src="soft-ui-dashboard-main/assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
         
@@ -153,10 +153,10 @@
       <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Dashboard</li>
+            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Paginas</a></li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page"> inicial</li>
           </ol>
-          <h6 class="font-weight-bolder mb-0">Dashboard</h6>
+          <h6 class="font-weight-bolder mb-0">Pagina Inicial</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
